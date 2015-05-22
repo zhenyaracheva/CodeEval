6aab7b166ff3110b9fa80a747ea10bd36c34ae79
zhenyaracheva <zhenia.racheva@gmail.com>
2015-05-22 09:01:24 +0300

    Tasks

---
+++ MthToLastElement/MthToLastElementSolver.cs
@@ -0,0 +1,35 @@
+﻿namespace MthToLastElement
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class MthToLastElementSolver
+    {
+       public static void Main(string[] args)
+        {
+            var output = new StringBuilder();
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            //using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                var line = reader.ReadLine();
+
+                while(line != null)
+                {
+                    var currentLine = line.Split(' ');
+                    var index = int.Parse(currentLine[currentLine.Length - 1]);
+                    
+                    if(index < currentLine.Length)
+                    {
+                        output.AppendLine(currentLine[currentLine.Length - index - 1]);
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            Console.WriteLine(output.ToString().Trim());
+        }
+    }
+}
