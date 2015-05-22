3707163c33776e1b84f50dac904049ac8b44a9dc
zhenyaracheva <zhenia.racheva@gmail.com>
2015-05-22 13:42:20 +0300

    Tasks

---
+++ CountingPrimes/CountingPrimesSolver.cs
@@ -0,0 +1,77 @@
+﻿namespace CountingPrimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    public class CountingPrimesSolver
+    {
+        public static void Main(string[] args)
+        {
+            var output = new StringBuilder();
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            //using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+
+                    var numbers = line.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    var from = int.Parse(numbers[0]);
+                    var to = int.Parse(numbers[1]);
+
+                    output.AppendLine(FindPrimes(to, from).ToString());
+
+
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            Console.WriteLine(output.ToString().Trim());
+        }
+
+        private static int FindPrimes(int to, int from)
+        {
+            var primes = new List<int>();
+
+            for (int i = from; i <= to; i++)
+            {
+                if (isPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes.Count;
+        }
+
+        private static bool isPrime(int n)
+        {
+            int checkTo = (int)Math.Sqrt(n);
+            int counter = 0;
+
+            for (int i = 1; i <= checkTo; i++)
+            {
+                if (n % i == 0)
+                {
+                    counter++;
+                    if (counter >= 2)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            if (counter == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ NumberOfOnes/NumberOfOnes.cs
@@ -0,0 +1,38 @@
+﻿namespace NumberOfOnes
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class NumberOfOnes
+    {
+        public static void Main(string[] args)
+        {
+
+            var output = new StringBuilder();
+
+            using (StreamReader reader = File.OpenText(args[0]))
+            //using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    int number;
+                    bool result = Int32.TryParse(line, out number);
+                    if (result)
+                    {
+                        var binary = Convert.ToString(number, 2);
+
+                        output.AppendLine(binary.Count(x => x.Equals('1')).ToString());
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            Console.WriteLine(output.ToString().Trim());
+        }
+    }
+}
