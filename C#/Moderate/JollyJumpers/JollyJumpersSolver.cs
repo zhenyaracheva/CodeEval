47497f7c1d8bd326a8d6b579480e02d1dbb13158
zhenyaracheva <zhenia.racheva@gmail.com>
2015-06-18 15:39:24 +0300

    Solved

---
+++ C#/Moderate/JollyJumpers/JollyJumpersSolver.cs
@@ -0,0 +1,47 @@
+﻿namespace JollyJumpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class JollyJumpersSolver
+    {
+        public static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            // using (var reader = new StreamReader("input.txt"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (null == line)
+                    {
+                        continue;
+                    }
+
+                    var numbers = line.Split(' ').Select(int.Parse).ToArray();
+                    var n = numbers[0];
+                    var result = new HashSet<int>();
+                    var output = "Jolly";
+
+                    for (int j = 1; j < n; j++)
+                    {
+                        var num = Math.Abs(numbers[j] - numbers[j + 1]);
+                        if (num > 0 && num < n)
+                        {
+                            result.Add(num);
+                        }
+                    }
+
+                    if (result.Count != n - 1)
+                    {
+                        output = "Not jolly";
+                    }
+
+                    Console.WriteLine(output);
+                }
+            }
+        }
+    }
+}
---
+++ JavaScript/Moderate/jolly-jumping.js
@@ -0,0 +1,48 @@
+// var fs = require("fs");
+// fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+//     if (line != "") {
+//         var i,
+//             num,
+//             result = [],
+//             numbers = line.split(' ').map(Number),
+//             n = numbers[0];
+//
+//         for (i = 1; i < n; i += 1) {
+//             num = Math.abs(numbers[i] - numbers[i + 1]);
+//             if (result.indexOf(num) === -1 && num > 0 && num < n) {
+//                 result.push(num);
+//             }
+//         }
+//
+//         if (result.length === n - 1) {
+//             console.log('Jolly')
+//         } else {
+//             console.log('Not jolly');
+//         }
+//     }
+// });
+
+function jollyJumping(line) {
+    var i,
+        num,
+        result = [],
+        numbers = line.split(' ').map(Number),
+        n = numbers[0];
+
+    for (i = 1; i < n; i += 1) {
+        num = Math.abs(numbers[i] - numbers[i + 1]);
+        if (result.indexOf(num) === -1 && num > 0 && num < n) {
+            result.push(num);
+        }
+    }
+
+    if (result.length === n - 1) {
+        console.log('Jolly')
+    } else {
+        console.log('Not jolly');
+    }
+}
+
+console.log(jollyJumping('4 1 4 2 3'));
+console.log(jollyJumping('3 7 7 8'));
+console.log(jollyJumping('9 8 9 7 10 6 1'));
