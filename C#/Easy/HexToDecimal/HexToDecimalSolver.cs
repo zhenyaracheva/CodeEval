4ca215391677ee6c7d9d45b330e0398d84193caa
zhenyaracheva <zhenia.racheva@gmail.com>
2015-06-22 22:24:09 +0300

    Solved

---
+++ C#/Easy/HexToDecimal/HexToDecimalSolver.cs
@@ -0,0 +1,25 @@
+﻿namespace HexToDecimal
+{
+    using System;
+    using System.IO;
+
+    public class HexToDecimalSolver
+    {
+        public static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line =reader.ReadLine();
+                    if (null == line)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine(Convert.ToInt32(line,16));
+                }
+            }
+        }
+    }
+}
---
+++ JavaScript/Easy/hex-to-decimal.js
@@ -0,0 +1,6 @@
+function hexToDecimal(line){
+    console.log(parseInt(line,16));
+}
+
+hexToDecimal('9f');
+hexToDecimal('11');
